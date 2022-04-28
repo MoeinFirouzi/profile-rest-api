8131a09e399fc7c17255860a00c5c42068b1997b
MoeinFirouzi <48527672+MoeinFirouzi@users.noreply.github.com>
2022-04-28 18:13:22 +0430

    Just uncomment a forgotten line :)

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -34,7 +34,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    #objects = UserProfileManager()
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELD = ['name']
